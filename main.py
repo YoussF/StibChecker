fd774eaf742297e42960fedad665cfa452ab9994
Berge Maxim <44107024+GraphtyLove@users.noreply.github.com>
2022-05-27 08:49:38 +0200

    Update to python3

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python2
+"""File that scrap the STIB's website in order to get information about a line."""
 import urllib
 import os
 from xml.etree import ElementTree
 from time import sleep
+import urllib.request
 
-ligne = raw_input("Ligne : ")
-sense = raw_input("Sense : ")
+line = input("Line : ")
+direction = input("Direction : ")
+
 while True:
 	os.system('tput reset')
-	url = urllib.urlopen('http://m.stib.be/api/getitinerary.php?line={}&iti={}'.format(ligne, sense))
+	url = urllib.request.urlopen(f"http://m.stib.be/api/getitinerary.php?line={line}&iti={direction}")
 	feed = ElementTree.parse(url)
 	for stop in feed.findall('stop'):
 		if stop.findtext('present'):
-			print '[x] '+stop.findtext('name')
+			print(f"[x] {stop.findtext('name')}")
 		else:
-			print '[ ] '+stop.findtext('name')
-
+			print(f"[ ] {stop.findtext('name')}")
 	sleep(10)
